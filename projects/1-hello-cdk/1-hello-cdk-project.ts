87554a9880c13c1425f2a9bbad76fa3031439794
Sam Ward Biddle <71524487+samwardbiddle@users.noreply.github.com>
2023-06-29 13:55:06 +0000

    First commit. Yay! 🚀

---
+++ projects/1-hello-cdk/1-hello-cdk-project.ts
@@ -0,0 +1,26 @@
+/**
+ * In this project you will complete the 'hello world' equivalent for infrastructure as code.
+ * You will build and deploy a single stack, or collection of AWS services.
+ * Your stack will consist of an S3 bucket
+ */
+
+// import the dependencies
+
+
+
+// Declare the props interface
+// Explanation
+
+// Write your JSdocs comments
+
+// Instantiate the Stack object
+
+// Create the S3 bucket
+
+// Create the lambda function
+
+// Create the SNS notification
+
+// Declare your stack outputs
+
+// Add tags
